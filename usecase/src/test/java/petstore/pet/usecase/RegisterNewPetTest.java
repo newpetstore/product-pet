d5458bb2f9235c7391d56826475d81ebefae8abf
Fabio José <fabiojose@gmail.com>
2020-01-02 10:48:23 -0300

    :page_facing_up: Add apache-2.0 license header
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- usecase/src/test/java/petstore/pet/usecase/RegisterNewPetTest.java
+++ usecase/src/test/java/petstore/pet/usecase/RegisterNewPetTest.java
@@ -1,3 +1,18 @@
+/*
+ * Java (TM) Pet Store Modernized Edition - 2019
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package petstore.pet.usecase;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,7 +36,7 @@ import petstore.pet.usecase.exception.PetAlreadyExistsException;
 import petstore.pet.usecase.model.NewPet;
 import petstore.pet.usecase.model.PetCreated;
 import petstore.pet.usecase.port.CategoryDatastore;
-import petstore.pet.usecase.port.IdGenerator;
+import petstore.pet.usecase.port.PetIdGenerator;
 import petstore.pet.usecase.port.PetDatastore;
 
 /**
@@ -39,7 +54,7 @@ public class RegisterNewPetTest {
 	CategoryDatastore categories;
 	
 	@Mock
-	IdGenerator<String> generator;
+	PetIdGenerator generator;
 	
 	@InjectMocks
 	RegisterNewPet usecase;
